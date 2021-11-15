70650f647d69e46a9c06c318be414d1e9b65b202
Joe Streifel <josephstreifel@gmail.com>
2021-11-15 15:01:01 -0800

    adds test path for deployment testing

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const { response } = require('express');
 
 //setup express and middleware
 const app = express();
@@ -22,5 +23,9 @@ db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', function() {
   console.log('Mongoose is connected!');
 });
+
+//Request Paths
+app.get('/test', (req, res) => res.status(200).send('I AM ALIVE! Hello! :)'));
+
 
 app.listen(PORT, () => console.log(`I am alive! Listening on ${3001}`));

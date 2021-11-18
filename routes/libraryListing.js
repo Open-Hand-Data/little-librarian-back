5b78cce6a9acf0f9cf1e54a3c90551dffadcb846
Joe Streifel <josephstreifel@gmail.com>
2021-11-18 14:46:52 -0800

    fixes library search to function and include title

--- routes/libraryListing.js
+++ routes/libraryListing.js
@@ -4,20 +4,28 @@ require('dotenv').config();
 const Book = require('../Models/bookModel');
 const Library = require('../Models/libraryModel');
 
-async function findLibraries (arr) {
+async function findLibraries(arr) {
 
 }
 
 async function handleLibraryList(req, res) {
   try {
     let bookResults = await Book.find({
-      title: {$regex: req.query.title, $options: 'i'}
+      title: { $regex: req.query.title, $options: 'i' }
     });
-    let libraries = await Promise.all(bookResults.map(result => {
-      let libraryResult=Library.find({ charter: result.libraryCharter })
+    let libraries = await Promise.all(bookResults.map(async result => {
+      let libraryResult = await Library.find({ charter: result.libraryCharter })
       let library = libraryResult[0];
-      library.title = result.title;
-      return library;
+      let modLibrary = {
+        _id: library._id,
+        geolocation: library.geolocation,
+        libraryName: library.libraryName,
+        charter: library.charter,
+        title: result.title,
+        __v: library.__v
+      };
+      console.log(modLibrary);
+      return modLibrary;
     }))
     res.status(200).send(libraries);
   } catch (err) {
